2769c6349d328bcfd9b3e8835c7969fde2ea982f
wyon <wangyaodong5@163.com>
2015-01-06 17:00:56 +0800

    zipUtils

--- src/com/android/common/Tools.java
+++ src/com/android/common/Tools.java
@@ -1,10 +1,76 @@
 package com.android.common;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Environment;
 
 public final class Tools {
 	private Tools() {
+	}
+
+	/**
+	 * uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"
+	 * <p>
+	 * 判断是否有网络连接
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static boolean isNetworkConnected(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo mNetworkInfo = mConnectivityManager
+					.getActiveNetworkInfo();
+			if (mNetworkInfo != null) {
+				return mNetworkInfo.isAvailable();
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"
+	 * <p>
+	 * 判断WIFI网络是否可用
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static boolean isWifiConnected(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo mWiFiNetworkInfo = mConnectivityManager
+					.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+			if (mWiFiNetworkInfo != null) {
+				return mWiFiNetworkInfo.isAvailable();
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"
+	 * <p>
+	 * 判断手机网络是否可用
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public static boolean isMobileConnected(Context context) {
+		if (context != null) {
+			ConnectivityManager mConnectivityManager = (ConnectivityManager) context
+					.getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo mMobileNetworkInfo = mConnectivityManager
+					.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+			if (mMobileNetworkInfo != null) {
+				return mMobileNetworkInfo.isAvailable();
+			}
+		}
+		return false;
 	}
 
 	public static boolean isExternalStorageWritable() {
